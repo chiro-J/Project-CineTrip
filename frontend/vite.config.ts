abb1b7877836bc8a3222bdd0dd170baae1faa9ae
hm <gtg1349@gmail.com>
2025-09-08 11:21:55 +0900

    chore/frontend architecture

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/ //env불러오기 설정 추가
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     build: {
       outDir: "./dist", //build 파일 저장 위치
       emptyOutDir: true, // 전에 있던 빌드 파일 지우고 다시 생성 설정
